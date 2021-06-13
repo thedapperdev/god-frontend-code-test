2ebff69b6e867ac67df0cad7af2d4791b30b6dc7
Jordan Paisley <jordan@cypher.dev>
2021-06-13 14:19:18 +0100

    add carousel package

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import CarBlock, { Props as Car } from '../components/car';
 // @ts-ignore
 import { View } from 'vcc-ui';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
+
 // @ts-ignore
 import carsData from '../cars.json';
 
@@ -11,10 +14,23 @@ interface Props {
 
 const Home: any = () => {
   return (
-    <View direction='row'>
-      {carsData.map(c => <CarBlock {...c} />)}
+    // <View direction='row'>
+      <CarouselProvider
+        naturalSlideWidth={100}
+        naturalSlideHeight={125}
+        visibleSlides={4}
+        totalSlides={carsData.length}>
+          <Slider>
+          {carsData.map((c, i) => {
+            return (<Slide index={i}>
+              <CarBlock  {...c} />
+            </Slide>)
+          })}
+          </Slider>
+      </CarouselProvider>
+      
 
-    </View>
+    // </View>
   )
 }
 
