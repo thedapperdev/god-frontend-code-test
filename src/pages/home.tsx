57850dae4b282460e1d64c93aaf293bbb4894dc9
Jordan Paisley <jordan@cypher.dev>
2021-06-13 14:10:59 +0100

    display all cars

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CarBlock, { Props as Car } from '../components/car';
-
+// @ts-ignore
+import { View } from 'vcc-ui';
 // @ts-ignore
 import carsData from '../cars.json';
 
@@ -9,7 +10,12 @@ interface Props {
 }
 
 const Home: any = () => {
-  return carsData.map(c => <CarBlock {...c} />)
+  return (
+    <View direction='row'>
+      {carsData.map(c => <CarBlock {...c} />)}
+
+    </View>
+  )
 }
 
 export default Home;
